79aa7fdf70acc8e4fef2c7c6aacc428c84ff6c52
Aleksey Kurepin <lesha@kurepin.com>
2013-05-31 19:27:54 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zxevo::Application.config.secret_token = '39a02621c6402185b74a5ddc6a3430addaa6c23b8a8b8416b0a3ae141f4ffc192ec51b5c69d5aa538945085845e64faa0bd1848ba9cdb8723ad7848f324d580b'
