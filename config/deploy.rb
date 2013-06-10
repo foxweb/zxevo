f1a9a5a45fce5cd4897f072e8ff367d4cf1f1d15
Aleksey Kurepin <lesha@kurepin.com>
2013-06-10 14:36:02 +0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,9 @@
 # encoding: utf-8
+
+require 'puma/capistrano'
+require 'capistrano-rbenv'
+set :rbenv_ruby_version, '2.0.0-p195'
+
 default_run_options[:pty] = true
 
 set :application, 'zxevo'
@@ -7,6 +12,8 @@ set :user, 'foxweb'
 set :use_sudo, false
 set :deploy_to, "/home/foxweb/www/#{application}"
 set :deploy_via, :remote_cache
+set :shared_children, shared_children << 'tmp/sockets'
+set :stage, 'production'
 
 role :web, 'rediron.ru'
 role :app, 'rediron.ru'
@@ -30,21 +37,31 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
     run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
     run "ln -nfs #{shared_path}/log #{release_path}/log"
+    run "ln -nfs #{shared_path}/tmp/sockets #{release_path}/tmp/sockets"
+    run "ln -nfs #{shared_path}/db #{release_path}/db/sqlite"
   end
 
   task :precompile_assets do
     run "cd #{release_path}; bundle exec rake assets:precompile RAILS_ENV=production"
   end
 
-  task :start do
-    run "bundle exec pumactl -S #{deploy_to}/shared/tmp/puma.state start"
+  desc "Start the application"
+  task :start, roles: :app, except: { no_release: true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec puma -b 'unix://#{shared_path}/sockets/puma.sock' -S #{shared_path}/sockets/puma.state --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{stage}.log 2>&1 &", :pty => false
   end
-  
-  task :stop do
-    run "bundle exec pumactl -S #{deploy_to}/shared/tmp/puma.state stop"
+ 
+  desc "Stop the application"
+  task :stop, roles: :app, except: { no_release: true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stop"
   end
-
+ 
+  desc "Restart the application"
   task :restart, roles: :app, except: { no_release: true } do
-    run "bundle exec pumactl -S #{deploy_to}/shared/tmp/puma.state restart"
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state restart"
+  end
+ 
+  desc "Status of the application"
+  task :status, roles: :app, except: { no_release: true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stats"
   end
 end
